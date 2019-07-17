f8bb43002cc94e93feaec568bfaf35f8c5490a87
Jeffersson semin <jeffesemin@hotmail.com>
2019-07-16 23:45:11 -0300

    alterado modelo do projeto, adicionado alguns stryles e algumas funcoes de botoes para testar.

--- index.android.js
+++ index.android.js
@@ -1,53 +1,66 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from 'react';
+import { Text, AppRegistry, View, Button, Image, TouchableOpacity, Alert } from 'react-native';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-export default class reactApp1 extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
+const Estilos = {
+  principal: {
+    backgroundColor: '#000',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    paddingTop: 40
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  botao: {
+    paddingVertical: 10,
+    paddingHorizontal:40,
+    marginTop: 20,
+    backgroundColor: 'green',
+    padding: 10,
+    borderColor: '#000',
+    borderWidth: 1,
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.6
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  textoBotao: {
+    color: '#fff',
+    fontSize: 15,
+    fontWeight: 'bold',
+    alignSelf: 'center',
 
-AppRegistry.registerComponent('reactApp1', () => reactApp1);
+  },
+  imagem: {
+    justifyContent: 'flex-end',
+    padding: 5
+  }
+};
+
+const botaoPressionado = () => {
+var numeroAleatorio = Math.random();
+numeroAleatorio = Math.floor(numeroAleatorio * 5);
+
+var frases = Array();
+frases[0] = 'Boa sorte';
+frases[1] = 'Bom dia';
+frases[2] = 'Boa noite';
+frases[3] = 'Boa s';
+frases[4] = 'Tchau';
+
+var frasesEscolhidas = frases[numeroAleatorio];
+
+Alert.alert(frasesEscolhidas);
+}
+
+const App = () => {
+  const { principal, botao, textoBotao } = Estilos;
+  return (
+    <View style={principal}>
+      <Image source={require('./imgs/logo.png')} />
+      <TouchableOpacity style={botao} onPress={botaoPressionado}>
+        <Text style={textoBotao}>Nova frase</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+AppRegistry.registerComponent('reactApp1', () => App);
